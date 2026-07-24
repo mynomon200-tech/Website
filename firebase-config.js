4f20fec770307dddfa3b597bb8d8be73d40914ee
mynomon200-tech <mynomon200@gmail.com>
2026-07-24 22:20:48 +0200

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Paste your Web App config from Firebase Console → Project settings → Your apps
+window.FIREBASE_CONFIG = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'chatr-6d513.firebaseapp.com',
+  databaseURL: 'https://chatr-6d513-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'chatr-6d513',
+  storageBucket: 'chatr-6d513.firebasestorage.app',
+  messagingSenderId: 'YOUR_MESSAGING_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
